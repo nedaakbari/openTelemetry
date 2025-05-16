1d4e27a3ee5a4124e22dd0a125d6a47187061b87
nedaakbari <nedaakbari555@gmail.com>
2025-05-16 21:48:17 +0330

    update

---
+++ openTel/otelOne/one/src/test/java/org/example/one/OneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
